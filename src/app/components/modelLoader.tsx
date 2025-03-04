6956205113dada83bf4548e5e4cbe8ede29e0711
Kieran <kieran.caruana@gmail.com>
2025-03-04 23:58:50 +0000

    Setup ThreeJS canvas and created model loader

---
+++ src/app/components/modelLoader.tsx
@@ -0,0 +1,50 @@
+﻿'use client';
+
+import { useEffect } from 'react';
+import * as THREE from 'three';
+import { GLTF, GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
+
+import SceneInit from './sceneSetup';
+
+function ModelLoader() {
+    useEffect(() => {
+        const canvas = new SceneInit('myThreeJsCanvas');
+        canvas.initialize();
+        canvas.animate();
+
+        // const boxGeometry = new THREE.BoxGeometry(8, 8, 8);
+        // const boxMaterial = new THREE.MeshNormalMaterial();
+        // const boxMesh = new THREE.Mesh(boxGeometry, boxMaterial);
+        // test.scene.add(boxMesh);
+
+        let loadedModel: GLTF;
+        const glftLoader = new GLTFLoader();
+        glftLoader.load('/models/retro_computer/scene.gltf', (gltfScene) => {
+            loadedModel = gltfScene;
+            // console.log(loadedModel);
+
+            gltfScene.scene.rotation.y = Math.PI / 8;
+            gltfScene.scene.position.y = 3;
+            gltfScene.scene.scale.set(10, 10, 10);
+            canvas.scene.add(gltfScene.scene);
+        });
+
+        const animate = () => {
+            if (loadedModel) {
+                loadedModel.scene.rotation.x += 0.01;
+                loadedModel.scene.rotation.y += 0.01;
+                loadedModel.scene.rotation.z += 0.01;
+            }
+            requestAnimationFrame(animate);
+        };
+        animate();
+    }, []);
+
+    return (
+        <div>
+            <canvas id="myThreeJsCanvas" />
+        </div>
+    );
+}
+
+export default ModelLoader;
